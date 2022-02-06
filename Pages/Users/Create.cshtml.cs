699f1501517dccb38725aadf8ecf0869e7a8a34b
Laura Martinez <lauramartinezo182@gmail.com>
2022-02-06 11:02:18 -0500

    Added images

--- Pages/Users/Create.cshtml.cs
+++ Pages/Users/Create.cshtml.cs
@@ -17,9 +17,7 @@ namespace WebApp_RCP.Pages.Users
         private readonly WebApp_RCP.Data.WebApp_RCPContext _context;
         [BindProperty]
         public User User { get; set; }
-        public IList<User> UsersList { get; set; }
-
-        public string CheckPassword { get; set; }
+        
 
         public CreateModel(WebApp_RCP.Data.WebApp_RCPContext context)
         {
@@ -31,52 +29,13 @@ namespace WebApp_RCP.Pages.Users
             return Page();
         }
 
-        public async Task findUsername()
-        {
-            var users = from m in _context.User
-                             select m;
-            
-            if (!string.IsNullOrEmpty(User.UserName))
-            {
-                users = users.Where(s => s.UserName.Equals(User.UserName));
-            }
-
-            UsersList = await users.ToListAsync();
-
-            if (UsersList.Count == 1)
-            {
-                ViewData["Message"] = "This user is already registered on the platform, try again";
-            }
-        }
-
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync()
         {
-            ViewData["Message"] = null;
-
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
-            /*if (CheckPassword.Equals(User.Password))
-            {
-                await findUsername();
-
-                if (ViewData["Message"] == null)
-                {
-                    _context.User.Add(User);
-                    await _context.SaveChangesAsync();
-                    return RedirectToPage("../Index");
-                }
-            }
-
-            else
-            {
-                ViewData["Message"] = "Passwords don't match, try again";
-            }
-            return RedirectToPage("./Index");*/
-
             _context.User.Add(User);
             await _context.SaveChangesAsync();
             return RedirectToPage("../Index");
